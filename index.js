809532683a1b3485b2512dfbe22ed4afd606d1f8
Nishchay Arya <nishchay.arya92@gmail.com>
2020-07-30 15:28:10 -0400

    Fix vulnerability

--- index.js
+++ index.js
@@ -27,9 +27,40 @@ app.get("/", (req, res) => {
 app.get("/create", csrfProtection, (req, res) => {
   res.render("user-form", {
     title: "Create User",
-    csrfToken: req.csrfToken()
+    csrfToken: req.csrfToken(),
+    messages: []
   })
 })
+
+const validation = (req, res, next) => {
+  const { firstName, lastName, email, password, confirmedPassword } = req.body;
+  const errors = [];
+
+  if (!firstName) errors.push("Please provide a first name");
+  if (!lastName) errors.push("Please provide a last name");
+  if (!email) errors.push("Please provide an email");
+  if (!password) errors.push("Please provide a password");
+  if (password !== confirmedPassword) errors.push("The provided values for the password and password confirmation fields did not match.");  
+
+  req.errors = errors;
+  next();
+}
+
+app.post("/create", csrfProtection, validation, (req, res) => {
+  const { firstName, lastName, email, password, confirmedPassword } = req.body;
+  const errors = req.errors;
+
+  if (errors) {
+    res.render("user-form", {
+      title: "Create User",
+      csrfToken: req.csrfToken(),
+      messages: errors
+    })
+  }
+
+  users.push({ id: users.length + 1, firstName, lastName, email });
+  res.redirect("/");
+} )
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
